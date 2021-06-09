21c296918b79c2a1769d6afd3c0b95b71862f3ac
Ross Addinall <ross@vertizan.com>
2021-06-09 17:24:30 +0100

    Changed scripts to stop updating and creation of AUTHORS file on version update AUTHORS and CHANGELOG file deleted

--- package-scripts.js
+++ package-scripts.js
@@ -338,10 +338,10 @@ module.exports = {
       script: 'node scripts/linkify-changelog.js',
       description: 'Add/update GitHub links in CHANGELOG.md'
     }
-    // version: {
-    //   script:
-    //     'nps updateAuthors && nps linkifyChangelog && git add -A ./AUTHORS ./CHANGELOG.md',
-    //   description: 'Tasks to perform when `npm version` is run'
-    // }
+    version: {
+      // script: 'nps updateAuthors && nps linkifyChangelog && git add -A ./AUTHORS ./CHANGELOG.md',
+      script: 'node --version',
+      description: 'Tasks to perform when `npm version` is run - just print the version of node'
+    }
   }
 };
